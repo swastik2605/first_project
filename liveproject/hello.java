0b63f111897ccf8c813d4cf925528248f20eafc0
lakshya <lakshya10171@gmail.com>
2025-03-04 11:00:15 +0530

    updated

--- liveproject/hello.java
+++ liveproject/hello.java
@@ -2,5 +2,9 @@ package liveproject;
 public class hello{
     public static void main(String[] args) {
         System.out.println("Hello");
+        lak();
+    }
+    public static void lak(){
+        System.out.println("Hii swastik");
     }
 }
